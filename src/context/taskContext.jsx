eacfffea0a29f56b5d86d6e2613ebcb4b53dc32f
Omar Ezeldin <89748215+omar-ezeldin@users.noreply.github.com>
2023-07-18 10:43:04 +0300

    Added (Add Task)
    - Added the ability to add tasks.
    - Saved Tasks to local storage

--- src/context/taskContext.jsx
+++ src/context/taskContext.jsx
@@ -1,35 +1,46 @@
 import { createContext, useContext, useState } from "react";
 
 const TaskContext = createContext()
+const TaskAdd = createContext()
+const CurrTask = createContext()
 
 export function useTasks() {
     return useContext(TaskContext)
 }
 
+export function useTaskAdd() {
+    return useContext(TaskAdd)
+}
+
+
 
 export function TasksProvider({children}) {
+    const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || [])
 
-    const [tasks, setTasks] = useState([
-        {
-            title: "Task 1",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, excepturi.",
-            isCompleted: false,
-        },
-        {
-            title: "Task 2",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, excepturi.",
-            isCompleted: false,
-        },
-        {
-            title: "Task 3",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, excepturi.",
-            isCompleted: true,
-        }
-    ])
+    function addTask() {
+        setTasks(prevTasks => {
+            let newTodo = {
+                id: crypto.randomUUID(),
+                title: "Untitled Task",
+                description: "",
+                isCompleted: false
+            }
+            let tasks = [
+                newTodo,
+                ...prevTasks
+            ]
+            localStorage.setItem("tasks", JSON.stringify(tasks))
+            return tasks
+        })
+    }
 
     return (
-        <TaskContext.Provider value={tasks}>
-            {children}
-        </TaskContext.Provider>
+        <CurrTask.Provider value={null}>
+            <TaskAdd.Provider value={addTask}>
+                <TaskContext.Provider value={tasks}>
+                    {children}
+                </TaskContext.Provider>
+            </TaskAdd.Provider>
+        </CurrTask.Provider>
     )
 }
